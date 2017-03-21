156790011f9e7e05ad9ab316f5d5e35353ffa142
jack1197 <im-not-tiger@hotmail.com>
2017-03-22 12:16:01 +1300

    working on factory

--- COSC363A1/factory.cpp
+++ COSC363A1/factory.cpp
@@ -4,32 +4,41 @@
 
 Factory::Factory()
 {
-    floorTex = new Texture("TexturesCom_ConcreteBare0433_11_seamless_S.tga", "TGA");
-    wallTex = new Texture("TexturesCom_BrickSmallBrown0270_1_seamless_S.tga", "TGA");
+	floorTex = new Texture("TexturesCom_ConcreteBare0433_11_seamless_S.tga", "TGA");
+	wallTex = new Texture("TexturesCom_BrickSmallBrown0270_1_seamless_S.tga", "TGA");
 }
 
 
 Factory::~Factory()
 {
-    delete floorTex;
-    delete wallTex;
+	delete floorTex;
+	delete wallTex;
 }
 
 void Factory::Render()
 {
-    glPushMatrix();
+	glPushMatrix();
 	glScalef(100, 1, 100);
-	glTranslatef(-0.5, 0, -0.5);
+	glTranslatef(-0.5f, 0.f, -0.5f);
 	concreteFloor(100, 3);
-    glPopMatrix();
+	glPopMatrix();
 
-    glPushMatrix();
-    glTranslatef(0,25,-50);
-    glRotatef(90, 1, 0, 0);
+	glPushMatrix();
+	glTranslatef(0, 24.5, -50);
+	glRotatef(90, 1, 0, 0);
 	glScalef(100, 1, 50);
 	glTranslatef(-0.5, 0, -0.5);
 	brickWall(100, 3);
-    glPopMatrix();
+	glPopMatrix();
+
+	glPushMatrix();
+	glRotatef(90, 0, 1, 0);
+	glTranslatef(0, 24.5, -50);
+	glRotatef(90, 1, 0, 0);
+	glScalef(-100, 1, 50);
+	glTranslatef(-0.5, 0, -0.5);
+	brickWall(100, 3);
+	glPopMatrix();
 }
 
 void Factory::Process(double dt)
@@ -39,19 +48,19 @@ void Factory::Process(double dt)
 
 void Factory::brickWall(int subdivisions, int repetitions)
 {
-    wallTex->bind();
-    glColor3f(1,1,1);
-    glEnable(GL_TEXTURE_2D);
-    glBegin(GL_QUADS);
+	wallTex->bind();
+	glColor3f(1.f, 1.f, 1.f);
+	glEnable(GL_TEXTURE_2D);
+	glBegin(GL_QUADS);
 	subdividedSurface(subdivisions, repetitions);
-    glEnd();
-    glDisable(GL_TEXTURE_2D);
+	glEnd();
+	glDisable(GL_TEXTURE_2D);
 }
 
 void Factory::concreteFloor(int subdivisions, int repetitions)
 {
-    floorTex->bind();
-	glColor3f(1, 1, 1);
+	floorTex->bind();
+	glColor3f(1.f, 1.f, 1.f);
 	glEnable(GL_TEXTURE_2D);
 	glBegin(GL_QUADS);
 	subdividedSurface(subdivisions, repetitions);
@@ -61,21 +70,21 @@ void Factory::concreteFloor(int subdivisions, int repetitions)
 
 void Factory::subdividedSurface(int subdivisions, int repetitions)
 {
-for (int i = 0; i < subdivisions; i++)
+	for (int i = 0; i < subdivisions; i++)
 		for (int j = 0; j < subdivisions; j++)
 		{
 			float x1 = (float)i / (float)(subdivisions + 1);
-			float x2 = (float)(i+1) / (float)(subdivisions + 1);
+			float x2 = (float)(i + 1) / (float)(subdivisions + 1);
 			float y1 = (float)j / (float)(subdivisions + 1);
-			float y2 = (float)(j+1) / (float)(subdivisions + 1);
-			glNormal3f(0, 1, 0);
+			float y2 = (float)(j + 1) / (float)(subdivisions + 1);
+			glNormal3f(0.f, 1.f, 0.f);
 			glTexCoord2f(x1*repetitions, y1*repetitions);
-			glVertex3f(x1, 0, y1);
+			glVertex3f(x1, 0.f, y1);
 			glTexCoord2f(x1*repetitions, y2*repetitions);
-			glVertex3f(x1, 0, y2);
+			glVertex3f(x1, 0.f, y2);
 			glTexCoord2f(x2*repetitions, y2*repetitions);
-			glVertex3f(x2, 0, y2);
+			glVertex3f(x2, 0.f, y2);
 			glTexCoord2f(x2*repetitions, y1*repetitions);
-			glVertex3f(x2, 0, y1);
+			glVertex3f(x2, 0.f, y1);
 		}
 }
